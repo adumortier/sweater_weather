8e10b0272dcfd2d874a38efb25aa3e75887833a5
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-04-20 21:16:49 -0600

    Add tests for methods and attributes

--- spec/model/background_spec.rb
+++ spec/model/background_spec.rb
@@ -5,7 +5,17 @@ RSpec.describe Background do
   describe "attributes" do
     it "has a url and id attribute" do
       background = Background.new('http://google.com')
+      expect(background).to be_a_kind_of(Background)
       expect(background).to have_attributes(:id => "poro", :url => 'http://google.com')
     end
+  end
+
+  describe '#methods' do
+
+    it '#get_image_url' do
+      background = Background.get_image_url('Paris')
+      expect(background).to be_a_kind_of(Background)
+    end
+
   end
 end
