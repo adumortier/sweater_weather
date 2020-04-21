da149631654f5859ef5ab27de3fe4c917ca52951
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-04-20 22:10:29 -0600

    Pluralize background controller

--- spec/requests/api/v1/background_spec.rb
+++ spec/requests/api/v1/background_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe 'Background' do
 
   it 'sends a background image for the given location', :vcr do
-    get '/api/v1/background?location=denver,co'
+    get '/api/v1/backgrounds?location=denver,co'
     expect(response).to be_successful
     expect(response.status).to eq(200)
     result = JSON.parse(response.body, symbolize_names: true)
