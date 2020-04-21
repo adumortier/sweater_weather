5250a820661904218bb9a308ee86c5c328f5be25
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-04-21 10:57:29 -0600

    Add module for api connection

--- app/services/google_direction_service.rb
+++ app/services/google_direction_service.rb
@@ -1,19 +1,14 @@
 class GoogleDirectionService
 
+  extend Connectable
+
   def self.get_travel_time(origin, destination)
-    response = conn.get('directions/json?') do |req|
+    con = conn('https://maps.googleapis.com/maps/api', name: 'key', key: ENV['GOOGLE_API_KEY'] )
+    response = con.get('directions/json?') do |req|
       req.params['origin'] = origin
       req.params['destination'] = destination
     end
     JSON.parse(response.body, symbolize_names: true)[:routes].first[:legs].first[:duration][:value]
-  end
-
-  private
-
-  def self.conn
-    Faraday.new "https://maps.googleapis.com/maps/api" do |conn|
-      conn.params['key'] = ENV['GOOGLE_API_KEY']
-    end
   end
 
 end
