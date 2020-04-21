5ba229b5470862a5fb9dd8be7d7a190644c594ed
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-04-20 21:13:06 -0600

    Add status code expectations

--- spec/requests/api/v1/forecast_spec.rb
+++ spec/requests/api/v1/forecast_spec.rb
@@ -7,6 +7,7 @@ describe 'Forecast' do
     get '/api/v1/forecast?location=denver,co'
     expect(response).to be_successful
     result = JSON.parse(response.body, symbolize_names: true)
+    expect(response.status).to eq(200)
     expect(result[:data][:attributes].keys.include?(:weather_today_general)).to eq(true)
     expect(result[:data][:attributes].keys.include?(:weather_today_details)).to eq(true)
     expect(result[:data][:attributes].keys.include?(:weather_forecast)).to eq(true)
--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -6,7 +6,9 @@ describe 'Users' do
 
     post '/api/v1/users?email=whatever@example.com&password=password&password_confirmation=password'
     expect(response).to be_successful
+    expect(response.status).to eq(201)
     resp = JSON.parse(response.body)
+
     expect(resp['data']['attributes']['email']).to_not be_empty
     expect(resp['data']['attributes']['api_key']).to_not be_empty
   end
@@ -17,13 +19,15 @@ describe 'Users' do
 
     post '/api/v1/users?email=whatever@example.com&password=fakepassword&password_confirmation=fakepassword'
     expect(response).to_not be_successful
+    expect(response.status).to eq(400)
     resp = JSON.parse(response.body)
-    expect(resp['data']['attributes']['message']).to eq('Email and password credentials do not match')
 
+    expect(resp['data']['attributes']['message']).to eq('credentials do not match')
     post '/api/v1/users?email=whatever@example.com&password=password&password_confirmation=fakepassword'
     expect(response).to_not be_successful
+    expect(response.status).to eq(400)
     resp = JSON.parse(response.body)
-    expect(resp['data']['attributes']['message']).to eq('password and password confirmation do not match')
+    expect(resp['data']['attributes']['message']).to eq('password and confirmation do not match')
   end
 
 end
